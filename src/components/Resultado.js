ea8c42301d5bc8d737c640614be4491599859dd7
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-25 17:16:58 -0300

    fazendo visor no resultado

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Visor from './Visor'
 
 const Resultado =  props => (
-  <View>
-    <Text>Resultado</Text>
-  </View>
+  <Visor />
 );
 
 export { Resultado }
