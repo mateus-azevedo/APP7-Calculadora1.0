1f187d7d293dd84dc82488598fba4ca60051c688
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-25 16:49:08 -0300

    fazendo cabecalho no topo

--- src/components/Topo.js
+++ src/components/Topo.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Cabecalho from './Cabecalho';
 
 const Topo = props => (
-  <View>
-    <Text>Topo</Text>
-  </View>
+  <Cabecalho />
 );
 
 export { Topo };
