759653ce59dc984ad0cb83b819042e287f12c1cf
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-25 16:33:28 -0300

    criado components App, Topo, Painel, Resultado

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import React from 'react';
+import {AppRegistry, View} from 'react-native';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import App from './src/App';
+
+const app7 = () => (
+  <App />
+);
+
+AppRegistry.registerComponent(appName, () => app7);
