4b265524c036ce48521c9ea19a709009175a03c5
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-26 17:19:19 -0300

    Delegando variaveis de estado e funcoes via props para componentes filhos

--- src/components/Numero.js
+++ src/components/Numero.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
 export default props => (
-  <TextInput style={styles.numero} />
+  <TextInput style={styles.numero} value={props.num} />
 );
 
 const styles = StyleSheet.create({
--- src/components/Painel.js
+++ src/components/Painel.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 
 import Entrada from './Entrada';
 import Operacao from './Operacao';
 import Comando from './Comando';
 
-const Painel = props => (
-  <View>
-    <Entrada />
-    <Operacao />
-    <Comando />
-  </View>
-);
+class Painel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { num1: '10', num2: '25' };
+  }
+
+  calcular() {
+    console.log('Sim, vamos calcular!');
+  }
+  
+  render () {
+    return (
+      <View>
+        <Entrada num1={this.state.num1} num2={this.state.num2} />
+        <Operacao />
+        <Comando acao={this.calcular} />
+      </View>
+    );
+  }
+}
+
+// const Painel = props => (
+//   <View>
+//     <Entrada />
+//     <Operacao />
+//     <Comando />
+//   </View>
+// );
 
 export { Painel }
